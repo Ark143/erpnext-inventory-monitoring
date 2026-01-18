126dbf425736c029bb50df65c6b08b43710bb78d
JMIT <info@jmit.com>
2026-01-18 23:25:29 +0800

    Recreate proper Frappe app structure with correct hooks.py import and required files

--- inventory_monitoring/inventory_monitoring/doctype/inventory_dashboard/inventory_dashboard.py
+++ inventory_monitoring/inventory_monitoring/doctype/inventory_dashboard/inventory_dashboard.py
@@ -5,11 +5,6 @@ import frappe
 from frappe.model.document import Document
 
 class InventoryDashboard(Document):
-	def before_save(self):
-		if not self.created_by:
-			self.created_by = frappe.session.user
-		self.modified_by = frappe.session.user
-	
 	def get_stock_summary(self):
 		"""Get stock summary for dashboard"""
 		return frappe.db.sql("""
@@ -20,33 +15,3 @@ class InventoryDashboard(Document):
 			FROM `tabBin`
 			WHERE actual_qty > 0
 		""", as_dict=True)[0]
-	
-	def get_low_stock_items(self, limit=10):
-		"""Get items with low stock"""
-		return frappe.db.sql("""
-			SELECT 
-				b.item_code,
-				b.actual_qty,
-				i.item_name,
-				i.stock_uom,
-				COALESCE(i.reorder_level, 0) as reorder_level
-			FROM `tabBin` b
-			LEFT JOIN `tabItem` i ON b.item_code = i.name
-			WHERE b.actual_qty <= COALESCE(i.reorder_level, 0)
-			AND b.actual_qty >= 0
-			ORDER BY b.actual_qty ASC
-			LIMIT %s
-		""", (limit,), as_dict=True)
-	
-	def get_stock_movement_trend(self, days=30):
-		"""Get stock movement trend for specified days"""
-		return frappe.db.sql("""
-			SELECT 
-				DATE(posting_date) as date,
-				SUM(CASE WHEN actual_qty > 0 THEN actual_qty ELSE 0 END) as inward,
-				SUM(CASE WHEN actual_qty < 0 THEN ABS(actual_qty) ELSE 0 END) as outward
-			FROM `tabStock Ledger Entry`
-			WHERE posting_date >= DATE_SUB(CURDATE(), INTERVAL %s DAY)
-			GROUP BY DATE(posting_date)
-			ORDER BY posting_date DESC
-		""", (days,), as_dict=True)
--- inventory_monitoring/inventory_monitoring/hooks.py
+++ inventory_monitoring/inventory_monitoring/hooks.py
@@ -1,4 +1,4 @@
-from . import __version__ as app_version
+from inventory_monitoring import __version__ as app_version
 
 app_name = "inventory_monitoring"
 app_title = "Inventory Monitoring"
@@ -6,6 +6,9 @@ app_publisher = "Ark143"
 app_description = "Advanced Inventory Monitoring for ERPNext v15"
 app_email = "your.email@example.com"
 app_license = "MIT"
+
+# Required for ERPNext
+required_apps = ["erpnext"]
 
 # Includes in <head>
 # ------------------
@@ -51,15 +54,6 @@ app_license = "MIT"
 # automatically create page for each record of this doctype
 # website_generators = ["Web Page"]
 
-# Jinja
-# ----------
-
-# add methods and filters to jinja environment
-# jinja = {
-#	"methods": "inventory_monitoring.utils.jinja_methods",
-#	"filters": "inventory_monitoring.utils.jinja_filters"
-# }
-
 # Installation
 # ------------
 
@@ -71,32 +65,6 @@ app_license = "MIT"
 
 # before_uninstall = "inventory_monitoring.uninstall.before_uninstall"
 # after_uninstall = "inventory_monitoring.uninstall.after_uninstall"
-
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
-
-# notification_config = "inventory_monitoring.notifications.get_notification_config"
-
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
-
-# permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
-
-# DocType Class
-# ---------------
-# Override standard doctype classes
-
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
 
 # Document Events
 # ---------------
@@ -130,72 +98,3 @@ app_license = "MIT"
 #		"inventory_monitoring.tasks.monthly"
 #	],
 # }
-
-# Testing
-# -------
-
-# before_tests = "inventory_monitoring.install.before_tests"
-
-# Overriding Methods
-# ------------------------------
-#
-# override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "inventory_monitoring.event.get_events"
-# }
-#
-# each overriding function accepts a `data` argument;
-# generated from the base implementation of the doctype dashboard,
-# along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-#	"Task": "inventory_monitoring.task.get_dashboard_data"
-# }
-
-# exempt linked doctypes from being automatically cancelled
-#
-# auto_cancel_exempted_doctypes = ["Auto Repeat"]
-
-# Ignore links to specified DocTypes when deleting documents
-# -----------------------------------------------------------
-
-# ignore_links_on_delete = ["Communication", "ToDo"]
-
-# Request Events
-# ----------------
-# before_request = ["inventory_monitoring.utils.before_request"]
-# after_request = ["inventory_monitoring.utils.after_request"]
-
-# Job Events
-# ----------
-# before_job = ["inventory_monitoring.utils.before_job"]
-# after_job = ["inventory_monitoring.utils.after_job"]
-
-# User Data Protection
-# --------------------
-
-# user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
-# ]
-
-# Authentication and authorization
-# --------------------------------
-
-# auth_hooks = [
-#	"inventory_monitoring.auth.validate"
-# ]
